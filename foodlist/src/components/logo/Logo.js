a248ec67a63b3ce6fb078a955130edce3a143c53
Manning Stinson <manning@manningstinson.com>
2022-07-14 08:55:36 -0500

    Added Expense folder
    Created an expenses folder and updated import paths

---
+++ foodlist/src/components/logo/Logo.js
@@ -0,0 +1,12 @@
+
+import { ReactComponent as Logo } from '../../components/logo/5d-logo.svg';
+
+function LogoMain() {
+  return (
+    <div className= "logo">
+      <Logo />
+    </div>
+  );
+}
+
+export default LogoMain
